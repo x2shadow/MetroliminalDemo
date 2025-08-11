7333f3c4621e1a52fb1d6d9a679b4b7566a5fcf8
X2SHADOW <hahandrew88@gmail.com>
2025-08-11 18:20:08 +0700

    Added CH1_Trigger1
    - Turning on Light in Second Tunnel
    - Starting animation of closing train doors

---
+++ Assets/_Project/Scripts/CH1_Trigger1.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CH1_Trigger1 : MonoBehaviour
+{
+    private bool hasTriggered = false;      // Чтобы не запускать повторно
+
+    [SerializeField] GameObject secondTunnelLight;
+    [SerializeField] Animation trainDoorClosingAnimation;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (hasTriggered) return;
+
+        if (other.CompareTag("Player"))
+        {
+            hasTriggered = true;
+            secondTunnelLight.SetActive(true);
+            trainDoorClosingAnimation.Play();
+        }
+    }
+}
