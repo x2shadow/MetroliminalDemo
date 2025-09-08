2bacbbbf5e1367cc769f90ea09b75103e2bddfb5
X2SHADOW <hahandrew88@gmail.com>
2025-09-08 12:35:51 +0700

    SoundTrigger now can be triggerred only once

--- Assets/_Project/Scripts/SoundTrigger.cs
+++ Assets/_Project/Scripts/SoundTrigger.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class SoundTrigger : MonoBehaviour
 {
     [SerializeField] string soundName;
-    
+
+    private bool hasTriggered = false;      // Чтобы не запускать повторно
+
     void OnTriggerEnter(Collider other)
     {
+        if (hasTriggered) return;
         AudioManager.Instance.PlaySound(soundName);
+        hasTriggered = true;
     }
 }
