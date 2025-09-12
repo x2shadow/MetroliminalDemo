71deac3c31caec1b420aa8e5b20d21d9d5880e18
X2SHADOW <hahandrew88@gmail.com>
2025-09-12 08:21:46 +0700

    Added signals for dialogues in cutscene2

--- Assets/_Project/Scripts/DialogueSystem/DialogInCutscene.cs
+++ Assets/_Project/Scripts/DialogueSystem/DialogInCutscene.cs
@@ -2,7 +2,11 @@ using UnityEngine;
 
 public class DialogInCutscene : MonoBehaviour
 {
-    public DialogueScript dialogueScript;   // Сценарий диалога
+    public DialogueScript dialogueScript1;   // Сценарий диалога
+    public DialogueScript dialogueScript2;
+    public DialogueScript dialogueScript3;
+    public DialogueScript dialogueScript4;
+    public DialogueScript dialogueScript5;
 
     DialogueRunner dialogueRunner;   // Ссылка на DialogueRunner
     PlayerController player;
@@ -13,7 +17,13 @@ public class DialogInCutscene : MonoBehaviour
         if (player == null) player = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
-    public void StartDialogue()
+    public void StartDialogue1() => StartDialogue(dialogueScript1);
+    public void StartDialogue2() => StartDialogue(dialogueScript2);
+    public void StartDialogue3() => StartDialogue(dialogueScript3);
+    public void StartDialogue4() => StartDialogue(dialogueScript4);
+    public void StartDialogue5() => StartDialogue(dialogueScript5);
+    
+    void StartDialogue(DialogueScript dialogueScript)
     {
         if (dialogueRunner != null && dialogueScript != null)
         {
