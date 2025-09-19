0f1e0aac00c7a2964ae7d5b06d981a507ee62967
X2SHADOW <hahandrew88@gmail.com>
2025-09-19 21:42:17 +0700

    Cutscene3 kinda done

--- Assets/_Project/Scripts/PlayerSignalSetPosition.cs
+++ Assets/_Project/Scripts/PlayerSignalSetPosition.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerSignalSetPosition : MonoBehaviour
 {
-    [SerializeField]
-    Transform position;
+    [SerializeField] Transform position;
+    [SerializeField] Transform position2;
 
     public void SetPosition()
     {
@@ -15,5 +15,15 @@ public class PlayerSignalSetPosition : MonoBehaviour
             transform.SetPositionAndRotation(position.position, position.rotation);
         }
         else transform.SetPositionAndRotation(position.position, position.rotation);
+
+    }
+    public void SetPosition2()
+    {
+        if (position2 == null)
+        {
+            position = GameObject.Find("EndPosition2").transform;
+            transform.SetPositionAndRotation(position2.position, position2.rotation);
+        }
+        else transform.SetPositionAndRotation(position2.position, position2.rotation);
     }
 }
